34ee504937b9cc16f660379f555ea9939b68308d
ImLiips <demoniaklolo@gmail.com>
2023-07-02 01:48:11 +0200

    Mise à jour 1

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let playOnce = true;
+const popupProjets1 = document.querySelector(".projet1");
+const popupProjets2 = document.querySelector(".projet2");
 
 window.addEventListener("scroll", () => {
   let scrollValue =
     (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+  // Popup projets
+  if (scrollValue > 0.45) {
+    popupProjets1.style.opacity = 1;
+    popupProjets1.style.transform = "none";
+    popupProjets2.style.opacity = 1;
+    popupProjets2.style.transform = "none";
+  }
 
   // Popup
   if (scrollValue > 0.85 && playOnce) {
